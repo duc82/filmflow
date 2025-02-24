cbe30c5c410841625bcfdaee9244111946dab2b0
duc82 <duccdht123@gmail.com>
2025-02-24 08:18:49 +0700

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,13 @@
+import MovieList from "../components/Home/MovieList";
 import { getMovies } from "../services/movieService";
 
 export default async function Home() {
   const recommendations = await getMovies("phim-moi");
+  console.log(recommendations);
 
-  return <></>;
+  return (
+    <>
+      <MovieList movies={recommendations.items} />
+    </>
+  );
 }
--- src/app/components/Home/MovieList.tsx
+++ src/app/components/Home/MovieList.tsx
@@ -1,3 +1,32 @@
-export default function MovieList() {
-  return <section>MovieList</section>;
+"use client";
+
+import { Movie } from "@/app/types/movie";
+import { useLocale } from "next-intl";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+export default function MovieList({ movies }: { movies: Movie[] }) {
+  const locale = useLocale();
+
+  return (
+    <section>
+      <Swiper slidesPerView={2}>
+        {movies.map((movie) => (
+          <SwiperSlide key={movie._id} className="rounded-lg">
+            <Image
+              src={`https://img.ophim.live/${movie.thumb_url}`}
+              alt={movie.origin_name}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-auto object-cover"
+            />
+            <div className="absolute bottom-4 px-4">
+              <h1>{locale === "en" ? movie.origin_name : movie.name}</h1>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
 }
