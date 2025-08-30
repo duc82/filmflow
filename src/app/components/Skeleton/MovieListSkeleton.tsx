b32efa914ff4dc31dc53664df76e707091540337
duc82 <duccdht123@gmail.com>
2025-08-30 15:57:21 +0700

    fix search 2

--- src/app/components/Skeleton/MovieListSkeleton.tsx
+++ src/app/components/Skeleton/MovieListSkeleton.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Filter from "../Movies/Filter";
 
 export default function MovieListSkeleton({ length }: { length: number }) {
@@ -10,7 +11,9 @@ export default function MovieListSkeleton({ length }: { length: number }) {
       <div className="mt-2 py-2 w-full">
         {/* Movie Category */}
         <div className="animate-pulse h-8 w-28 bg-slate-300 dark:bg-slate-700 mb-4 rounded-lg"></div>
-        <Filter defaultSortField="modified.time" type="type" />
+        <Suspense>
+          <Filter defaultSortField="modified.time" type="type" />
+        </Suspense>
 
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-6 lg:gap-8 py-5">
           {Array.from({ length: length }).map((_, index) => (
